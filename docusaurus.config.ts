078049a945337299a3a70cbc30e1fdb140caa1eb
thesysadmindev <chris@thesysadmin.dev>
2024-07-27 18:23:01 +1000

    Config Update
    Updated editUrl in docusaurus.config.ts to fix the Edit This Page link.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thesysadmindev/rpkihub/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thesysadmindev/rpkihub/tree/main/',
         },
         blog: false,
         theme: {
